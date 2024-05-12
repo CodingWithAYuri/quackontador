a7c2d194eedd524fc3973ca6d3f8c412f5febcf8
AYuri <my.email@is.private>
2024-05-12 20:32:48 +0200

    Updates

--- src/App.test.js
+++
@@ -1,8 +0,0 @@
-import { render, screen } from '@testing-library/react';
-import App from './App';
-
-test('renders navigation link to calculations page', () => {
-  render(<App />);
-  const linkElement = screen.getByText(/Vamos aos cálculos!/i);
-  expect(linkElement).toBeInTheDocument();
-});
