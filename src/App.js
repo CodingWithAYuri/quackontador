684db1af5ca5f9bf8a6d029accb8afe953f6cde8
AYuri <my.email@is.private>
2025-06-05 19:25:26 -0300

    feat: adiciona rotas para as páginas de termos de uso e  política de privacidade

--- src/App.js
+++ src/App.js
@@ -1,33 +1,39 @@
 import React from 'react';
 import { HashRouter, Routes, Route } from 'react-router-dom';
-import { Container } from 'react-bootstrap';
 import Header from './components/Header';
 import Main from './components/Main';
 import Footer from './components/Footer';
 import ContactForm from './components/ContactForm';
 import Calculadora from './components/Calculadora';
-import CoverContainer from './components/CoverContainer'; 
 import LoginForm from './components/LoginForm';
 import SignUpForm from './components/SignUpForm';
 import Logout from './components/Logout';
+import TermosDeUso from './components/TermosDeUso';
+import PrivacyPolicy from './components/PrivacyPolicy';
 
 function App() {
   return (
     <HashRouter>
-      <CoverContainer style={{ display: 'flex', flexDirection: 'column', minHeight: '100vh' }}> 
-        <Container style={{ flex: 1, display: 'flex', flexDirection: 'column' }}>
-          <Routes>
-            {/* Rotas sem cabeçalho e sem rodapé */}
-            <Route path="/login" element={<LoginForm />} />
-            <Route path="/logout" element={<Logout />} />
-            <Route path="/cadastro" element={<SignUpForm />} />
-            {/* Rotas com cabeçalho e rodapé */}
-            <Route path="/" element={<WithHeaderAndFooter><Main /></WithHeaderAndFooter>} />
-            <Route path="/contactForm" element={<WithHeaderAndFooter><ContactForm /></WithHeaderAndFooter>} />
-            <Route path="/calculos" element={<WithHeaderAndFooter><Calculadora /></WithHeaderAndFooter>} />
-          </Routes>
-        </Container>
-      </CoverContainer> 
+      <div style={{ 
+        display: 'flex', 
+        flexDirection: 'column', 
+        minHeight: '100vh',
+        maxHeight: '100vh',
+        overflow: 'hidden'
+      }}>
+        <Routes>
+          {/* Rotas sem cabeçalho e sem rodapé */}
+          <Route path="/login" element={<LoginForm />} />
+          <Route path="/logout" element={<Logout />} />
+          <Route path="/cadastro" element={<SignUpForm />} />
+          {/* Rotas com cabeçalho e rodapé */}
+          <Route path="/" element={<WithHeaderAndFooter><Main /></WithHeaderAndFooter>} />
+          <Route path="/contactForm" element={<WithHeaderAndFooter><ContactForm /></WithHeaderAndFooter>} />
+          <Route path="/calculos" element={<WithHeaderAndFooter><Calculadora /></WithHeaderAndFooter>} />
+          <Route path="/termos-de-uso" element={<WithHeaderAndFooter><TermosDeUso /></WithHeaderAndFooter>} />
+          <Route path="/politica-de-privacidade" element={<WithHeaderAndFooter><PrivacyPolicy /></WithHeaderAndFooter>} />
+        </Routes>
+      </div> 
     </HashRouter>  
   );
 }
