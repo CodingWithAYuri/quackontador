0aa6bcebf36ee11ec6b61c1d70a5b0eca0eaf81c
AYuri <my.email@is.private>
2025-06-05 18:57:39 -0300

    chore: atualiza ano do footer para 2025

--- src/components/Footer.js
+++ src/components/Footer.js
@@ -6,15 +6,13 @@ function Footer() {
     <footer className="mastfoot mt-auto">
       <Row className="justify-content-center align-items-center">
         <Col xs="auto" className="text-center py-2">
-          QuackContador 2024 &copy;
-          <span className="ms-2 heart-symbol">&#x2763;</span>
-          <a href="https://github.com/CodingWithAYuri" className="ms-2 text-decoration-none opacity-50">CodingWithAYuri</a>
+            QuackContador 2025 &copy;
+          <span className="mx-2 heart-symbol">&#x2763;</span>
+          <a href="https://github.com/CodingWithAYuri" className="text-decoration-none">CodingWithAYuri</a>
         </Col>
       </Row>
     </footer>
   );
 }
-
-    
 
 export default Footer;
